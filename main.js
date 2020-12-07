46556ab3fd35a95f6cab018e375eb98354cfa845
CalvinTheGreatestCoder <73121416+CalvinTheGreatestCoder@users.noreply.github.com>
2020-12-07 18:46:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+canvas= document.getElementById ('my canvas')
+ctx= canvas.getcontect ("2d") 
+car_width= 100
+car_length = 90
+mars_image = "th.jpg";
+car_image = "red-top-car-png-12.png";
+car_x = 10
+car_y = 10
+function add) }
+background_imgTag = new image 
+background_imgTag = uploadBackground  
+background_imgTag = backgroundImage
+rover_imgTag = new image 
+rover_imgTag = uploadcar  
+background_imgTag = carImage
+
+function.uploadBackground) }
+ctx.drawimage (background_imgTag) 0, 0, canvas.width canvas.height
+function.uploadrover) }
+ctx.drawrover (car_imgTag) car_x car_y car.width car.height
